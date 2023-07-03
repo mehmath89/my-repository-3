882a588d1a9867a41e5b69bcb2ed02ffab05783d
Mehmet <mehmath89@gmail.com>
2023-07-03 00:11:02 -0400

    jinja templates

--- flask-01-02-hello-world-app-jinja-template/flask-02-jinja-template/jinja.py
+++ flask-01-02-hello-world-app-jinja-template/flask-02-jinja-template/jinja.py
@@ -1,13 +1,25 @@
 # - Import Flask and render_template modules.
+from flask import Flask, render_template
 
 # - Create an object named `app` from imported Flask module.
+app = Flask(__name__)
 
-# - Create an `index.html` file under templates folder.
+# - Create a function named `head` which sends number `number1` and `number2` 
+# variables to the `index.html`. Use these variables into the `index.html` file. 
+# Assign a URL route the `head` function with decorator `@app.route('/')`.
+@app.route('/')
+def head():
+    return render_template('index.html', number1=10, number2=20)
 
-# - Create a function named `head` which sends number `number1` and `number2` variables to the `index.html`. Use these variables into the `index.html` file. Assign a URL route the `head` function with decorator `@app.route('/')`.
+# 2. way
+@app.route('/number1/<string:num1>/number2/<string:num2>')
+def num(num1, num2):
+    return render_template('index.html', number1=num1, number2=num2)
 
-# - Create an `body.html` file under templates folder.
-
-# - Create a function named `number` which sends number `num1` and `num2` and sum of them to the `index.html`. Use these variables into the `body.html` file. Assign a URL route the `number` function with decorator `@app.route('/sum')`.
+# - Create a function named `number` which sends number `num1` and `num2` and sum of 
+# them to the `index.html`. Use these variables into the `body.html` file. Assign a 
+# URL route the `number` function with decorator `@app.route('/sum')`.
 
 # - run the application in debug mode
+if __name__ == '__main__':
+    app.run(debug=False)
