79aebffdfb238f9870a68f1c133576d7a1826421
Mehmet <mehmath89@gmail.com>
2023-07-04 17:14:40 -0400

    change index.html

---
+++ 101-kittens-carousel-static-website-ec2/userdata.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+yum update -y
+yum install httpd -y
+cd /var/www/html
+wget https://raw.githubusercontent.com/mehmath89/my-repository-3/main/101-kittens-carousel-static-website-ec2/static-web/index.html
+wget https://raw.githubusercontent.com/mehmath89/my-repository-3/main/101-kittens-carousel-static-website-ec2/static-web/cat0.jpg
+wget https://raw.githubusercontent.com/mehmath89/my-repository-3/main/101-kittens-carousel-static-website-ec2/static-web/cat1.jpg
+wget https://raw.githubusercontent.com/mehmath89/my-repository-3/main/101-kittens-carousel-static-website-ec2/static-web/cat2.jpg
+
+systemctl start httpd
